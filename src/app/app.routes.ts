4d58382fd29fd97183868b4e0648a3698bddb42d
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-07-11 23:03:55 +0530

    Add Contact Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { VehicleDetail } from './feature/vehicle/vehicle-detail/vehicle-detail';
 import { AddVehicle } from './feature/vehicle/add-vehicle/add-vehicle';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
     {
         path:'home',
         component:Home,
     },
+    {
+        path:'contact',
+        component:Contact
+    }
+    ,
     {
         path:'vehicle/:id',
         component:VehicleDetail
